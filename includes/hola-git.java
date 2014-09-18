80b789d4d21eef475d2ed3dfadfc0cab424751e8
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2014-09-18 11:20:06 -0400

    Update hola.git to enforce consistent braces in code.

--- includes/hola-git.java
+++ includes/hola-git.java
@@ -3,8 +3,7 @@
  * testing a quick code edit using the Github interface.. seems to work.
  */
 
-public class HolaGit
-{
+public class HolaGit {
 	public static void main(String[] args) {
 		System.out.println("Hola, Git!");
 	}
