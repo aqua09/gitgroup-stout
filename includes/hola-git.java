090d48a63786e2ae4818f8c0783ea7f15c2076e6
skassis <skassis@AIT100515.isc-ait.upenn.edu>
2014-10-09 09:35:28 -0400

    Added a new method

--- includes/hola-git.java
+++ includes/hola-git.java
@@ -5,5 +5,10 @@ public class HolaGit
 {
 	public static void main(String[] args) {
 		System.out.println("Hola, Git!");
+		displayMessage();
+	}
+	
+	public void displayMessage(){
+	  System.out.println("Git is so much better than CVS ......");
 	}
 }
